517c269f789a2651e68d35eed9a925eab184c951
Keeththigan <keeththi2003@gmail.com>
2026-08-26 01:50:51 +0530

    feat: add initial migration files for database schema and constraints

---
+++ prisma/migrations/20260825194756_updated_at_defaults/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "event_types" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "events" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
