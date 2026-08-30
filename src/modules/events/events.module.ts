fdb4cb45ed136f220ed2538957536f47443b1404
Keeththigan <keeththi2003@gmail.com>
2026-08-30 14:03:49 +0530

    fix: add PublicEventsController to EventsModule for enhanced event handling

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { PublicEventsController } from './public-events.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [EventsController],
+  controllers: [EventsController, PublicEventsController],
   providers: [EventsService],
   exports: [EventsService],
 })
