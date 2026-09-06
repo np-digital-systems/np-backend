03c8160edc07e908da8c28cc30462496734323a2
Keeththigan <keeththi2003@gmail.com>
2026-09-06 21:04:01 +0530

    feat: streamline auth service by removing user service dependency and enhancing identity handling

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
-import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PermissionsService } from './permissions.service';
@@ -10,7 +9,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { TokenService } from './token.service';
 
 @Module({
-  imports: [PassportModule.register({ session: false }), JwtModule.register({}), UsersModule],
+  imports: [PassportModule.register({ session: false }), JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, TokenService, PermissionsService, JwtStrategy],
   exports: [AuthService, PermissionsService],
